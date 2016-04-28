a1ff1354b9cf34cdc69bd547d529a7843c197597
Ivan <ivansaur@gmail.com>
2016-04-28 02:16:51 -0400

    all logic complete, need cleanup and print to file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <string>
-#include <array>
+
 
 using namespace std;
 ifstream in_data1;
@@ -25,219 +24,226 @@ public:
         cout << "Number of Nodes: " << numNodes;
 
         //STEP 1//
-        int temp = 0;
         loadCostMatrix(data);
-        sourceNode = 1;  //rotate through all source nodes
+        sourceNode = 1;  //Begin source node at 1
 
-        //STEP 2//
-        loadBestCostAry(sourceNode);
-        loadFatherAry(sourceNode);
-        loadMarkedAry();
+        //STEP 9-10//
+        while(sourceNode<=numNodes) {
+            cout<<endl<<endl<<"Source Node: "<<sourceNode<<endl<<endl;
+            Dijkstra(sourceNode);
+            sourceNode++;
+        }
 
-        debugPrint();
 
-        markMinNode(sourceNode-1);
+    }
 
-        while(!allMarked()) {
+        void printShortestPath(int node) {
+            //TEMP INT TO HOLD ORIGINAL VALUE OF fatherAry[node], in order to iterate through all nums
+            int temp = fatherAry[node];
 
-            minNode = findMin();
-            markMinNode(minNode);
+            cout << node + 1 << " <-- ";
+            while (fatherAry[node] != sourceNode - 1) {
+                cout << fatherAry[node] + 1 << " <-- ";
+                fatherAry[node] = fatherAry[fatherAry[node]];
+            }
+            cout << sourceNode << endl;
 
-            debugPrint();
+            //RESTORE ORIGINAL VALUE FOR NEXT ITERATION
+            fatherAry[node]=temp;
 
-            for (int k = 0; k < numNodes; k++) {
-                if (markedAry[k] == 0) { currentNode = k; }
-                newCost = computeCost(minNode, currentNode);
+        }
 
-                if (newCost < bestCostAry[currentNode]) {
-                    changeFather(currentNode, minNode);
-                    changeCost(currentNode, newCost);
-                    debugPrint();
+
+        bool allMarked(){
+            for(int i = 0; i<numNodes; i++){
+                if(markedAry[i] == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        int findMin() {
+            int minVal = 9999;
+            int minIndex = 0;
+            for(int i =0;i<numNodes;i++) {
+                if(bestCostAry[i]<minVal && markedAry[i]!=1) {
+                    minVal = bestCostAry[i];
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+        }
+
+        void debugPrint() {
+
+            cout<<endl<<endl<<"Father Array:";
+            printAry(fatherAry);
+            cout<<endl<<endl<<"Best Cost Array:";
+            printAry(bestCostAry);
+            cout<<endl<<endl<<"Marked Array:";
+            printAry(markedAry);
+
+            //    - debugPrint method, this method
+            //    Print sourceNode to output-2 (with proper heading, ie., the sourceNode is: )
+            //    Print fatherAry to output-2 (with proper heading)
+            //    Print bestCostAry to output-2 (with proper heading)
+            //    Print markedAry to output-2 (with proper heading)
+            //    - printShorestPaths (sourceNode)
+
+        }
+
+        void testPrint() {
+            cout<<endl<<endl<<"      1    2    3    4    5    6    7    8";
+            for(int i=0; i<numNodes; i++) {
+                cout<<endl<<endl<<i+1<<"  ";
+                for(int k=0; k<numNodes; k++) {
+                    cout<<setw(4)<<costMatrix[i][k]<<" ";
+                }
+            }
+            cout<<endl<<endl;
+        }
+
+        void printAry(int ary[]) {
+            cout<<endl;
+            for(int i=0;i<numNodes;i++) {
+                cout<<ary[i]<<" ";
+            }
+        }
+
+        void loadCostMatrix(ifstream& data) {
+
+            int row = 0;
+            int col = 0;
+            int cost = 0;
+
+            costMatrix = new int*[numNodes]();
+
+            for(int i = 0; i < numNodes; ++i)  {
+                costMatrix[i] = new int[numNodes];
+
+                for(int k=0;k<numNodes; ++k) {
+
+                    costMatrix[i][k]= 9999;
+                    costMatrix[i][i] = 0;
                 }
 
             }
+
+            while(!data.eof()) {
+
+                data>>row;
+                data>>col;
+                data>>cost;
+                costMatrix[row-1][col-1] = cost;
+
+            }
         }
 
+        void loadBestCostAry(int sourceNode) {
 
-    }
+            bestCostAry= new int[numNodes];
 
-    //Print out cost matrix for debugging
-
-    // trace from currentNode back to sourceNode (via fatherAry),
-    // and print the shortest path from sourceNode to currentNode  with the cost to output-1 (with proper heading)
-
-    void printShortestPath(int sourceNode) {
-
-      //  while()
-     //   cout<<
-
-
-    }
-
-
-    bool allMarked(){
-        for(int i = 0; i<numNodes; i++){
-            if(markedAry[i] == 0)
-                return false;
-        }
-        return true;
-    }
-
-    int findMin() {
-        int minVal = 9999;
-        int minIndex = 0;
-        for(int i =0;i<numNodes;i++) {
-            if(bestCostAry[i]<minVal && markedAry[i]!=1) {
-                minVal = bestCostAry[i];
-                minIndex = i;
+            for(int k=0;k<numNodes;k++) {
+                bestCostAry[k] = costMatrix[sourceNode-1][k];
             }
 
         }
-        return minIndex;
-    }
-    void debugPrint() {
+        //- loadBestCostAry (sourceNode) // copy the row of source node from costMatrix,
 
-        cout<<endl<<endl<<"Father Array:";
-        printAry(fatherAry);
-        cout<<endl<<endl<<"Best Cost Array:";
-        printAry(bestCostAry);
-        cout<<endl<<endl<<"Marked Array:";
-        printAry(markedAry);
 
-    }
+        void loadFatherAry (int sourceNode) {
 
-    void testPrint() {
-        cout<<endl<<endl<<"      1    2    3    4    5    6    7    8";
-        for(int i=0; i<numNodes; i++) {
-            cout<<endl<<endl<<i+1<<"  ";
-            for(int k=0; k<numNodes; k++) {
-                cout<<setw(4)<<costMatrix[i][k]<<" ";
+            fatherAry = new int[numNodes];
+            for(int i=0;i<numNodes;i++) {
+                fatherAry[i] = sourceNode-1;
             }
         }
-        cout<<endl<<endl;
-    }
 
-    void printAry(int ary[]) {
-        cout<<endl;
-        for(int i=0;i<numNodes;i++) {
-            cout<<ary[i]<<" ";
-        }
-    }
+        void loadMarkedAry() {
 
-    void loadCostMatrix(ifstream& data) {
-
-        int row = 0;
-        int col = 0;
-        int cost = 0;
-
-        costMatrix = new int*[numNodes]();
-
-        for(int i = 0; i < numNodes; ++i)  {
-            costMatrix[i] = new int[numNodes];
-
-            for(int k=0;k<numNodes; ++k) {
-
-                costMatrix[i][k]= 9999;
-                costMatrix[i][i] = 0;
+            markedAry = new int[numNodes];
+            for(int i=0;i<numNodes;i++) {
+                markedAry[i] = 0;
             }
+            //    - loadMarkedAry ( ) // set all to 0,
 
         }
 
-        while(!data.eof()) {
+        int computeCost (int minNode, int currentNode) { //the fuck
 
-            data>>row;
-            data>>col;
-            data>>cost;
-            costMatrix[row-1][col-1] = cost;
-
-        }
-    }
-
-    void loadBestCostAry(int sourceNode) {
-
-        bestCostAry= new int[numNodes];
-
-        for(int k=0;k<numNodes;k++) {
-            bestCostAry[k] = costMatrix[sourceNode-1][k];
+            //    - computeCost (minNode, currentNode) method,
+            //it compute the currentNode’s cost ( bestCostArray[minNode] +  cost from minNode to currentNode, in costMatrix)
+            return bestCostAry[minNode] + costMatrix[minNode][currentNode];
         }
 
-    }
-    //- loadBestCostAry (sourceNode) // copy the row of source node from costMatrix,
+        void markMinNode (int minNode) {
 
+            markedAry[minNode] = 1;
+            //    - markMinNode (minNode) // mark the minNode  in markedAry
 
-    void loadFatherAry (int sourceNode) {
-
-        fatherAry = new int[numNodes];
-        for(int i=0;i<numNodes;i++) {
-            fatherAry[i] = sourceNode-1;
         }
-    }
 
-    void loadMarkedAry() {
+        void changeFather ( int node, int minNode) {
 
-        markedAry = new int[numNodes];
-        for(int i=0;i<numNodes;i++) {
-            markedAry[i] = 0;
+            fatherAry[node] = minNode;
+            //    - changeFather (node, minNode) // set node’s father be minNode in fatherAry
+
         }
-        //    - loadMarkedAry ( ) // set all to 0,
 
-    }
+        void changeCost( int node, int newCost) {
 
-    int computeCost (int minNode, int currentNode) { //the fuck
+            bestCostAry[node] = newCost;
 
-        //    - computeCost (minNode, currentNode) method,
-        //it compute the currentNode’s cost ( bestCostArray[minNode] +  cost from minNode to currentNode, in costMatrix)
-        return bestCostAry[minNode] + costMatrix[minNode][currentNode];
-    }
+        }
 
-    void markMinNode (int minNode) {
+        void Dijkstra(int sourceNode) {
 
-        markedAry[minNode] = 1;
-        //    - markMinNode (minNode) // mark the minNode  in markedAry
+            //STEP 2//
+            loadBestCostAry(sourceNode);
+            loadFatherAry(sourceNode);
+            loadMarkedAry();
 
-    }
+            debugPrint();
 
-    void changeFather ( int node, int minNode) {
+            //STEP 3-6//
+            markMinNode(sourceNode-1);
+            while(!allMarked()) {
 
-        fatherAry[node] = minNode;
-        //    - changeFather (node, minNode) // set node’s father be minNode in fatherAry
+                minNode = findMin();
+                markMinNode(minNode);
 
-    }
+                debugPrint();
 
-    void changeCost( int node, int newCost) {
+                for (int k = 0; k < numNodes; k++) {
+                    if (markedAry[k] == 0) { currentNode = k; }
+                    newCost = computeCost(minNode, currentNode);
 
-        bestCostAry[node] = newCost;
+                    if (newCost < bestCostAry[currentNode]) {
+                        changeFather(currentNode, minNode);
+                        changeCost(currentNode, newCost);
+                        debugPrint();
+                    }
 
-    }
+                }
+            }
+            cout<<endl<<endl;
 
-    void Dijkstra() {
+            //STEP 7-8//
+            currentNode = 0;
+            while(currentNode<numNodes) {
+                printShortestPath(currentNode);
+                currentNode++;
+            }
+        }
 
-        //    - Dijkstras’ method // the Dijkstra’s algorithm
+    };
 
-    }
+    int main(int argc, char *argv[]) {
+        in_data1.open(argv[1]);
+        out_data1.open(argv[2]);
+        out_data2.open(argv[3]);
 
+        DijkstraSSS dij(in_data1);
 
-//    - debugPrint method, this method
-//    Print sourceNode to output-2 (with proper heading, ie., the sourceNode is: )
-//    Print fatherAry to output-2 (with proper heading)
-//    Print bestCostAry to output-2 (with proper heading)
-//    Print markedAry to output-2 (with proper heading)
-//    - printShorestPaths (sourceNode)
-
-
-};
-
-int main(int argc, char *argv[]) {
-    in_data1.open(argv[1]);
-    out_data1.open(argv[2]);
-    out_data2.open(argv[3]);
-
-    DijkstraSSS dij(in_data1);
-    //dij.testPrint();
-
-
-
-    cout << "\n\nEND" << endl;
-    return 0;
-
-};
+        return 0;
+    };
